bfee4f2f8d9f27061b2eb7ba716442bd437efaf0
sleepycat-glitch <asleepycat15@gmail.com>
2026-01-27 14:58:25 +0000

    functions

--- 3_return.py
+++ 3_return.py
@@ -1,6 +1,11 @@
 # Return Practice #1
 # Create a function called power that takes two numeric values as arguments. It must return the number that results from solving a power, using the first number as the base, and the second as the exponent
 
+def power(a,b):
+    c = a**b
+    return c
+
+print(power(3,5))
 
 
 
@@ -14,8 +19,13 @@
 
 # Hint: to perform the conversion, the function internally must multiply this value in dollars by 0.90 to obtain the equivalent amount in euros.
 
+dollars = 284.38
 
+def usd_to_eur(dollars):
+    eur = dollars * 0.9
+    return eur
 
+print(usd_to_eur(dollars))
 
 # Return Practice #3
 # Create a function called reverse_word that takes the characters of a given word as an argument, reverses the order of their characters, and returns them that way and in uppercase.
@@ -25,3 +35,14 @@
 # Also, you must create a variable called word, which contains any string, to pass it as an argument to the created function.
 
 # Hint: inside the created function, you should use string methods already seen.
+
+word = "pineapple"
+
+def reverse_word(word):
+    word = list(word)
+    word.reverse()
+    word = ''.join(word)
+    word = word.upper()
+    return word
+
+print(reverse_word(word))
