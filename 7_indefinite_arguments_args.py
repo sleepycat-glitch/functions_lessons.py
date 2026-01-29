a8fe70f6a5a51636b6536e7afb869e706c51dfba
sleepycat-glitch <asleepycat15@gmail.com>
2026-01-29 14:41:31 +0000

    functions

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,3 +1,22 @@
+def tea_order(customer_name, tea_type,*args, **kwargs): 
+    print(f"{customer_name} ordered a {tea_type}.")
+    for arg in args:
+        print("- Add :", arg)
+    for key,value in kwargs.items():
+        print("- Add", key, ":", value)
+
+tea_order("Alice","chamomile") 
+tea_order("Bob", "black", milk = "oat")
+tea_order("Tony", "black", "oat", sweetener = "honey")
+# can only accept a finite number of arguments for finite number of parameters
+# *args allows for an indefinite amount of argumemts since it continously creates new parameters
+# *args acts as empty tuple, unable to handle keyword arguments (ex. milk = "oat")
+# **kwargs allows for keyword arguments, acts as empty dictionary
+# can have both *args and **kwargs in code, but *args must come first (positional arguments before keyword arguments)
+
+
+
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
